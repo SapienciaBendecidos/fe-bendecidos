bb4343649cc39aef34f1a1ebde16cf1a964532e1
coco-napky <coco.napky@gmail.com>
2016-09-16 18:20:07 -0600

    User Service
    Added User Service
    Added login to User Service

---
+++ app/js/services/user.js
@@ -0,0 +1,28 @@
+import  { apiUrl } from '../constants';
+
+function UserService($http) {
+  'ngInject';
+
+  const service = {};
+
+  // credentials: { email, passowrd}
+  service.login = function(credentials) {
+
+    let url = `${apiUrl}users/login`;
+
+    return new Promise((resolve, reject) => {
+      $http.post(url, credentials).success((data) => {
+        resolve(data);
+      }).error((err, status) => {
+        reject(err, status);
+      });
+    });
+  };
+
+  return service;
+}
+
+export default {
+  name: 'UserService',
+  fn: UserService
+};
