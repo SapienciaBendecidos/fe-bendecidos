a06af4f043fdfafa3c17d3cdff83fb1ad9245fa8
coco-napky <coco.napky@gmail.com>
2016-09-23 19:46:28 -0600

    Redirect on login success

--- app/js/controllers/login.js
+++ app/js/controllers/login.js
@@ -2,7 +2,7 @@
 
 import loginMaterialize from '../jQuery/views/login';
 
-function LoginController(UserService, SessionService, $timeout) {
+function LoginController(UserService, SessionService, $timeout, $state) {
   'ngInject';
   // ViewModel
   const vm = this;
@@ -10,6 +10,7 @@ function LoginController(UserService, SessionService, $timeout) {
   vm.password = '';
   vm.loginFailed = false;
   vm.loading     = false;
+  vm.errorMessage = $state.params.redirected ? 'Inicie sesión para continuar' : '';
 
   vm.login = (email, password) =>{
     let credentials = {email, password};
@@ -36,8 +37,9 @@ function LoginController(UserService, SessionService, $timeout) {
   };
 
   vm.showLoader = () => {
-    vm.loginFailed = false;
-    vm.loading     = true;
+    vm.errorMessage = '';
+    vm.loginFailed  = false;
+    vm.loading      = true;
   }
 
   vm.hideLoader = loginFailed =>
@@ -47,10 +49,13 @@ function LoginController(UserService, SessionService, $timeout) {
 
         //TODO: redirect on login success
         if(!loginFailed)
-          window.alert('Login success, take me somewhere else pl0x :v');
+          $state.go('Client');
+        else
+          vm.errorMessage = 'Correo o contraseña invalida';
       }, 1250);
 
   loginMaterialize.init();
+  console.log($state);
 }
 
 export default {
