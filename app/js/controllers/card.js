fe3b3b61f95af5a3e87b5b8f75ead6a7df9057b6
coco-napky <coco.napky@gmail.com>
2017-01-23 22:59:58 -0600

    update tarjeta

--- app/js/controllers/card.js
+++ app/js/controllers/card.js
@@ -1,17 +1,33 @@
 'use strict';
 
-function CardController(client, cards) {
+function CardController(client, cards, CardService) {
 	'ngInject';
 	let vm = this;
 	vm.client = client.data;
  	vm.cards = cards.data;
 	vm.name = `${vm.client.primerNombre} ${vm.client.primerApellido}`;
 	vm.focusedCard = null;
+	vm.state = false;
 
-	vm.setFocus = card =>  vm.focusedCard = card;
+	vm.setFocus = card =>  {
+		vm.focusedCard = card;
+		vm.state = vm.focusedCard.estado == 'Activo';
+	};
 
 	vm.edit = () => {
-		console.log('edit');
+		let estado = vm.state ? 'Activo' : 'Inactivo';
+		let data = Object.assign({}, vm.focusedCard);
+		data.estado = estado;
+		let promise = CardService.updateCard(data);
+
+		promise.then(() => Materialize.toast('Tarjeta modificada'));
+		promise.catch(() => Materialize.toast('Error al guardar tarjeta'));
+		promise.finally(() => vm.loadCards());
+	}
+
+	vm.loadCards = () => {
+		let promise = CardService.getCardsByClient(vm.focusedCard.idCliente);
+		promise.then(response => vm.cards = response.data);
 	}
 }
 
---
+++ app/js/services/tarjetas.js
@@ -0,0 +1,20 @@
+'use strict';
+
+import  { apiUrl } from '../constants';
+
+function CardService($http) {
+  'ngInject';
+
+  const service = {};
+  service.getCards = () => $http.get(`${apiUrl}tarjetas`);
+  service.getCardsByClient = clientId => $http.get(`${apiUrl}clientes/${clientId}/tarjetas`);
+
+  //{ idTarjeta, estado ...}
+  service.updateCard = data => $http.patch(`${apiUrl}tarjetas`, data);
+  return service;
+}
+
+export default {
+  name: 'CardService',
+  fn: CardService
+};
