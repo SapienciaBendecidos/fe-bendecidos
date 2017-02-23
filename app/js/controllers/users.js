0f3682fa3311d6977c2e427471cc38a6a55c4f08
kenystev <kjestevez.0795@gmail.com>
2017-02-23 11:04:40 -0600

    show users and create user

--- app/js/controllers/users.js
+++ app/js/controllers/users.js
@@ -1,7 +1,136 @@
 'use strict';
 
-function UsersController(ClientService, $q) {
+function UsersController(UserService, $q) {
   'ngInject';
+  // ViewModel
+  const vm = this;
+  vm.users = [];
+  vm.pages = 0;
+  vm.perPage = 100;
+  vm.range = [];
+  vm.activePage = 0;
+  vm.focusedUser = {};
+  vm.userForm = {};
+  vm.roles = UserService.getRoles();
+  console.log(vm.roles);
+
+  vm.getFullName = (user) => {
+    return user.firstName + ' ' + user.firstSurname;
+  };
+
+  vm.setFocusedUser = id => vm.focusedUser = vm.getUserById(id);
+
+  vm.edit = () => {
+    let promise = vm.postUser(vm.focusedUser);
+    promise.then(() => {
+      Materialize.toast('Usuario editado exitosamente',5000);
+      vm.loadUsers();
+    });
+    promise.catch(() => {
+      Materialize.toast('Error al editar usuario',5000);
+      vm.loadUsers();
+    });
+  }
+
+  vm.delete = () => {
+    let promise = vm.deleteById(vm.focusedUser.id);
+    promise.then(() => {
+      Materialize.toast('Usuario eliminado exitosamente', 5000);
+      vm.loadUsers();
+    });
+    promise.catch(() => {
+      Materialize.toast('Error al eliminar usuario', 5000);
+      vm.loadUsers();
+    });
+  }
+
+  vm.deleteById = id => UserService.deleteById(id);
+
+  vm.getUserById = id => {
+    for (let i = 0; i < vm.users.length; ++i)
+      if(vm.users[i].id == id) {
+          let user = vm.users[i];
+          return  {
+            id: user.id,
+            firstName: user.firstName,
+            secondName: user.secondName,
+            firstSurname: user.firstSurname,
+            secondSurname: user.secondSurname,
+            email: user.email,
+          }
+      }
+  }
+
+  vm.getUsers = (page) => {
+    let skip = page * vm.perPage,
+        limit = vm.perPage;
+    return UserService.getUsers({limit,skip});
+  };
+
+  vm.loadUsers = () =>  vm.getUsers(vm.activePage)
+  .then(response => vm.users = response.data);
+
+  vm.handlePageControlClick = (event) => {
+    let active = event.currentTarget.attributes['data-control'].value;
+    if(active.toLowerCase() === 'false')
+    return;
+
+    let control = event.currentTarget.attributes['data-control'].value;
+    let page = parseInt(vm.activePage);
+    vm.activePage = control === 'foward' ? page + 1 : page - 1;
+    vm.loadUsers();
+  }
+
+  vm.handlePageIndexClick = (event) => {
+    vm.activePage = event.currentTarget.attributes['data-index'].value;
+    vm.loadUsers();
+  }
+
+  vm.getUserCount = () => UserService.countUsers();
+  vm.postUser = (user) => UserService.postUser(user);
+
+  vm.submitUser = () => {
+    let { firstname, middlename, lastname, secondLastname, email, rol, newPassword, newUsername } = vm.post;
+    let user = {
+      firstName: firstname,
+      secondName: middlename ? middlename : '',
+      firstSurname: lastname,
+      secondSurname: secondLastname ? secondLastname : '',
+      email: email,
+      type: rol,
+      emailVerified: true,
+      status: 'disable',
+      password:newPassword,
+      username: newUsername
+    };
+
+    console.log(JSON.stringify(user))
+
+    let promise = vm.postUser(user);
+    promise.then(() => {
+      Materialize.toast('Usuario agregado exitosamente', 5000);
+      vm.loadUsers();
+    });
+    promise.catch(() => {
+      Materialize.toast('Error al agregar usuario', 5000);
+      vm.loadUsers();
+    });
+    vm.post = {};
+  }
+
+  let getCount   = vm.getUserCount(),
+  getUsers = vm.getUsers(0);
+
+  $q.all([getCount, getUsers]).then( responses => {
+    let { count } = responses[0].data;
+    vm.pages = Math.ceil(count/vm.perPage);
+    vm.range = [];
+
+    for (let i = 0; i < vm.pages; i++)
+      vm.range.push(i);
+
+    vm.users = responses[1].data
+  });
 }
 
 export default {
--- app/js/services/user.js
+++ app/js/services/user.js
@@ -8,9 +8,9 @@ function UserService($http) {
   const service = {};
 
   // credentials: { email, passowrd }
-  service.login = credentials => $http.post(`${apiUrl}users/login`, credentials);
+  service.login = credentials => $http.post(`${apiUrl}Users/login`, credentials);
 
-  service.getUserById = id => $http.get(`${apiUrl}users/${id}`, id);
+  service.getUserById = id => $http.get(`${apiUrl}Users/${id}`, id);
 
   service.logout = () => $http.post(`${apiUrl}Users/logout`);
 
@@ -18,9 +18,11 @@ function UserService($http) {
 
   service.getUsers = filter => {
     if(!filter)
-      return $http.get(`${apiUrl}Users`);
-    return $http.get(`${apiUrl}Users?filter=${JSON.stringify(filter)}`);
+      return $http.get(`${apiUrl}Users?filter[include]=roles`);
+    return $http.get(`${apiUrl}Users?filter[include]=roles&filter=${JSON.stringify(filter)}`);
   }
+
+  service.getRoles = () => {return ['⁠⁠⁠admin', 'cajero', 'movil'];}
 
   service.postUser = (user) => $http.post(`${apiUrl}Users/replaceOrCreate`, user);
   service.deleteById = id => $http.delete(`${apiUrl}Users/${id}`);
